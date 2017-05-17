b4dbf8f320a9d3cd49afabc5c071cd4ff6aecabc
pancheco <pacheco.neto11@hotmail.com>
2017-05-16 23:43:26 -0300

    Seguradora

---
+++ Cliente.java
@@ -0,0 +1,14 @@
+package br.unipe.cc.erros.seguradora;
+
+public abstract class Cliente {
+	public String cliente;
+	public float valor_imovel;
+	public float seguro;
+	public String endereco;
+	public int zona;
+	public int tipo;
+	public int numero_funcionarios;
+	public int numero_visitas;
+	public int ramo;
+
+}
